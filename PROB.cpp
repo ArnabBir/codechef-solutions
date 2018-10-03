0e06f4e3b82261f5bf0ce6d77b7d663f9dc2f811
Arnab Bir <arnabbir@gmail.com>
2018-10-03 11:06:22 +0530

    Create PROB.cpp

---
+++ PROB.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	long a, b, c, d;
+	cin>>t;
+	while(t--) {
+		cin>>a>>b>>c>>d;
+		cout<<(((double) a) / (double)(a+b)) <<endl; 
+	}
+  return 0;
+}
